b11da7f954eff33c7770fe29d2d9996e498d4896
Dylan Bailey <wearesleepless@gmail.com>
2023-09-14 12:05:25 -0400

    Functional delete

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
-
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("leads"))
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("leads"))
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     renderLeads()
 }
 
+// Save Input
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("leads", JSON.stringify(myLeads))
+    if (inputEl.value != "") {
+        myLeads.push(inputEl.value)
+        inputEl.value = ""
+        localStorage.setItem("leads", JSON.stringify(myLeads))
+        renderLeads()
+    }
+})
+
+// Delete All
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
     renderLeads()
 })
 
